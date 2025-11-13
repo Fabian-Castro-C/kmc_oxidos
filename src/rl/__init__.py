93f20a1a7448d5468c4f6750a48bf959e985d190
Fabián Castro <fab.castro2001@gmail.com>
2025-11-13 04:28:06 -0300

    feat: add observation functions for local extraction in SwarmThinkers

--- src/rl/__init__.py
+++ src/rl/__init__.py
@@ -2,8 +2,11 @@
 
 from .critic import CriticNetwork, create_critic_network
 from .environment import TiO2GrowthEnv
+from .observations import get_batch_observations, get_diffusable_atoms, get_local_observation
 from .policy import ActorNetwork, create_policy_network
 from .reweighting import ImportanceSampler, ReweightingMechanism
+from .swarm_engine import SwarmEngine, SwarmProposal
+from .swarm_policy import DiffusionSwarmPolicy, create_diffusion_swarm_policy
 
 __all__ = [
     "TiO2GrowthEnv",
@@ -13,4 +16,11 @@ __all__ = [
     "create_critic_network",
     "ReweightingMechanism",
     "ImportanceSampler",
+    "get_local_observation",
+    "get_batch_observations",
+    "get_diffusable_atoms",
+    "DiffusionSwarmPolicy",
+    "create_diffusion_swarm_policy",
+    "SwarmEngine",
+    "SwarmProposal",
 ]
